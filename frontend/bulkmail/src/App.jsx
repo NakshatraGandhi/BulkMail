f778375a7adb0cd1acc3808163c87599165f9f23
NakshatraGandhi <nakshath2003@gmail.com>
2025-06-22 15:01:15 +0530

    Updated with Render

--- frontend/bulkmail/src/App.jsx
+++ frontend/bulkmail/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   function send()
   {
     setstatus(true)
-    axios.post("http://localhost:5000/sendemail",{msg:msg,emailList:emailList})
+    axios.post("https://bulkmail-2-8fhm.onrender.com/sendemail",{msg:msg,emailList:emailList})
     .then(function(data)
     {
       if(data.data === true)
